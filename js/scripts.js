74460aed5e42939b17b1e7b48000015ace4ce9eb
ladegbaye Stella <Ladegbayeewatomi50@gmail.com>
2022-05-09 14:39:53 +0100

    adding my initial file html

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#inputs").submit(function (e) {
+    e.preventDefault();
+
+
+    const firstNumber = parseInt($("#A").val());
+    const secondNumber = parseInt($("#B").val());
+    const thirdNumber = parseInt($("#C").val());
+
+
+
+
+
+
+  })
+
+
+
+
+
+
+});
