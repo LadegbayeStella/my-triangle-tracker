5b2ffbdbf0995061f2eab07999b685545ee5835b
ladegbaye Stella <Ladegbayeewatomi50@gmail.com>
2022-05-12 07:35:49 +0100

    fixed a bug in my css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("#inputs").submit(function (e) {
     e.preventDefault();
-
+    // $(".isosceles").show();
 
     const firstNumber = parseInt($("#A").val());
     const secondNumber = parseInt($("#B").val());
@@ -9,10 +9,26 @@ $(document).ready(function () {
 
 
 
+    if (firstNumber === secondNumber && firstNumber === thirdNumber && secondNumber === thirdNumber) {
+      $(".Equilateral").fadeToggle(4000);
+    }
+
+else if ((firstNumber + secondNumber) <= thirdNumber || (firstNumber + thirdNumber) <= secondNumber || (secondNumber + thirdNumber) <= firstNumber ) {
+      $(".nat").show();
+    }
+
+    else if (firstNumber === secondNumber || firstNumber === thirdNumber || secondNumber === thirdNumber) {
+      $(".isosceles").fadeToggle();
+    }
+  
+    else if (firstNumber !== secondNumber || firstNumber !== thirdNumber || secondNumber !== thirdNumber) {
+      $(".SCALENE").fadeToggle();
+    }
+    
 
 
 
-  })
+  });
 
 
 
